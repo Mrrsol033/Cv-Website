ef2702d59d19e95256a94c8548c90543a1b7884b
mrrsol033 <mrrsol.it@gmail.com>
2025-07-04 10:08:48 +0700

    add

---
+++ script/style.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+        theme: {
+          extend: {
+            fontFamily: {
+              'matangi': ['Georgia', 'serif']
+            },
+            spacing: {
+              '30': '7.5rem',
+              '70': '17.5rem',
+              '100': '25rem',
+              '107': '26.75rem',
+              '130': '32.5rem',
+              '236': '59rem',
+              '50': '12.5rem',
+              '90': '22.5rem'
+            }
+          }
+        }
+      }
